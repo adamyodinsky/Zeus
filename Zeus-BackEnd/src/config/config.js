9032646d75a5cf6b93a51c6867d408402d4c7b8f
Adam Yodinsky <adam@zooz.com>
2020-01-18 16:21:04 +0200

    pagination - cleaning sum s3 things

--- Zeus-BackEnd/src/config/config.js
+++ Zeus-BackEnd/src/config/config.js
@@ -1,13 +1,10 @@
 'use strict';
 
 module.exports = {
-  Host: process.env.CONSUL_HOST || 'localhost',
-  Port: Number(process.env.CONSUL_PORT || 3001),
+  Host: process.env.HOST || 'localhost',
+  Port: Number(process.env.PORT || 3001),
   logLevel: process.env.LOG_LEVEL || 'info',
   DEBUG: process.env.DEBUG === "true",
-  STATE_FILE_PATH: process.env.STATE_FILE_PATH || `${__dirname}/stateFiles`,
-  STATE_FILE_NAME: process.env.STATE_FILE_NAME || 'ZeusState',
-  S3_BUCKET: process.env.S3_BUCKET,
   //----------FRONT-END----------------------------
   frontEndHost: process.env.FRONTEND_HOST || 'localhost',
   frontEndPort: Number(process.env.FRONTEND_PORT) || 3006,
--- Zeus-BackEnd/src/controllers/getStateControler.js
+++ Zeus-BackEnd/src/controllers/getStateControler.js
@@ -6,9 +6,8 @@ const { exec } = require('../helpers/exec');
 
 const getState = async(req, res) => {
   try {
-    const statePath = req.query.stateType === 'demo'? `${config.STATE_FILE_PATH}/demo_state.json` : `${config.STATE_FILE_PATH}/${config.STATE_FILE_NAME}`;
-    const state = JSON.parse(fs.readFileSync(statePath));
-    res.status(200).json(state);
+
+    res.status(200).json("OK");
     logger.info('get state controller success');
   } catch (e) {
     logger.error(e.message)
@@ -26,27 +25,4 @@ const health = async(req, res) => {
 };
 
 
-const downloadFromS3 = async() => {
-  logger.info(`- Loading State File: '${config.STATE_FILE_NAME}' From S3 Bucket...`);
-
-  try {
-    await exec(
-        `aws  --profile=zooz-dev s3 ls s3://${config.S3_BUCKET}/${config.STATE_FILE_NAME}`,
-        true);
-    await exec(
-        `aws  --profile=zooz-dev s3 cp s3://${config.S3_BUCKET}/${config.STATE_FILE_NAME} ${config.STATE_FILE_PATH}/${config.STATE_FILE_NAME}`,
-        true);
-    console.log('Loaded state file successfully!');
-  } catch (e) {
-    console.log('Could not load state file from s3 bucket. Creating new local state file');
-    await exec(`echo '[]' > ${config.STATE_FILE_PATH}/${config.STATE_FILE_NAME}`);
-  }
-};
-
-const downloadFromS3Interval = () => {
-  downloadFromS3();
-  setInterval(downloadFromS3, 1000*30);
-};
-
-
-module.exports = { health, getState, downloadFromS3Interval };
+module.exports = { health, getState };
