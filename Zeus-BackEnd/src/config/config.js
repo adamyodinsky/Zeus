1ff232571d151a00cf24694cb882b4228f9825a6
Adam Yodinsky <adam@zooz.com>
2020-01-18 17:53:38 +0200

    building pagination with regex query and sorting limit etc...

--- Zeus-BackEnd/src/config/config.js
+++ Zeus-BackEnd/src/config/config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+  deploymentModelName: process.env.DEPLOYMENT_MODEL || 'deployments',
   Host: process.env.HOST || 'localhost',
   Port: Number(process.env.PORT || 3001),
   logLevel: process.env.LOG_LEVEL || 'info',
@@ -8,4 +9,5 @@ module.exports = {
   //----------FRONT-END----------------------------
   frontEndHost: process.env.FRONTEND_HOST || 'localhost',
   frontEndPort: Number(process.env.FRONTEND_PORT) || 3006,
+  MONGO_URI: process.env.MONGO_URI || "mongodb://root:1234@localhost:27017/admin"
 };
--- Zeus-BackEnd/src/controllers/getStateControler.js
+++ Zeus-BackEnd/src/controllers/getStateControler.js
@@ -1,16 +1,37 @@
-const logger = require('../helpers/logger');
-const config = require('../config/config');
+const config = require("../config/config");
+const logger = require("../helpers/logger");
+const { createConditions } = require('../helpers/createCondition');
+const { DeploymentSchema } = require("../models/Deployment");
+const DeploymentModel = require("mongoose").model(
+  config.deploymentModelName,
+  DeploymentSchema
+);
 
-
-const getState = async(req, res) => {
+const getState = async (req, res) => {
   try {
+    const limit = req.query.limit || 25;
+    const page = req.query.page || 0;
+    const sort = req.query.sort || "";
+    const regexOptions = req.query.regexOpt || 'i';
 
-    res.status(200).json("OK");
-    logger.info('get state controller success');
+    // set regex
+    let regex = req.query.regex || "";
+    regex = new RegExp(`${regex}`);
+
+    // set conditions
+    const fields = ['cluster', 'namespace', 'deployment_name', 'containers.container_name'];
+    const conditions = createConditions(regex, fields, regexOptions);
+
+    const response = await DeploymentModel.find(conditions)
+      .limit(limit)
+      .skip(page * limit)
+      .sort(sort);
+
+    res.status(200).json(response);
+    logger.info("get state controller success");
   } catch (e) {
-    logger.error(e.message)
+    logger.error(e.message);
   }
 };
-
 
 module.exports = { getState };
