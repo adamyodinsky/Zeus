928c138f8860734468ae6ecb06a923b3946efcd7
Adam Yodinsky <adam.promo@gmail.com>
2020-03-01 23:56:27 +0200

    nodes page

--- zeus-frontend/src/Components/Nodes/Nodes.js
+++ zeus-frontend/src/Components/Nodes/Nodes.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Nodes = (props) => {
-    return(
-        <div></div>
-    )
+class Nodes extends React.Component  {
+    render () {
+        return (
+            <div>
+                NodesPage
+            </div>
+        )
+    }
 };
 
 export default Nodes;
