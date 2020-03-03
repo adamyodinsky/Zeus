383ec874bd3aed8168d2773f5dd74497403f13af
Adam Yodinsky <adam@zooz.com>
2020-03-03 14:05:33 +0200

    seperating node scraping and deployment scraping - node scraper works

--- Zeus-DeploymentsScraper/src/helpers/saveToMongo.js
+++ Zeus-DeploymentsScraper/src/helpers/saveToMongo.js
@@ -1,12 +1,7 @@
-const {
-  CurrentUsage,
-  currentUsageModelName
-} = require("../models/CurrentUsage");
+const {CurrentUsage, currentUsageModelName} = require("../models/CurrentUsage");
 const CurrentUsageModel = require("mongoose").model(currentUsageModelName);
 const { Deployment, deploymentModelName } = require("../models/Deployment");
 const DeploymentModel = require("mongoose").model(deploymentModelName);
-const { Node, nodeModelName, NodeSchema } = require("../models/Node");
-const NodeModel = require("mongoose").model(nodeModelName);
 
 
 const logger = require("../helpers/logger");
@@ -99,70 +94,5 @@ const saveDeployment = async newDeployment => {
 };
 
 
-const saveNode = async newNode => {
-  let count;
-  let newNodeDoc;
-  let conditions = [
-    { name: newNode.name }
-  ];
 
-  try {
-    let nodeExists = await Node.findOne({ $and: conditions });
-    if (!nodeExists) {
-      newNodeDoc = new Node({ ...newNode });
-      count = await newNodeDoc.save();
-    } else {
-      // update surface objects
-      count = await NodeModel.updateOne(
-          { $and: conditions },
-          {
-            $set: {
-              last_update: Date.now(),
-              updates_counter: nodeExists.updates_counter + 1,
-              pods: newNode.pods
-              // TODO expirationDate: Date.now() + 1000 * 60 * config.SAVE_DOC
-            },
-            $push: { "node": newNode.node[0] }
-          },
-          { new: true }
-      );
-
-    }
-    logger.debug("Stored Node Object in DB", null);
-  } catch (err) {
-    logger.error(err.stack);
-  }
-  return 1;
-};
-
-
-const saveNodeUsage = async newNodeUsage => {
-  let count;
-  let conditions = [
-    { name: newNodeUsage.name }
-  ];
-
-  try {
-    let nodeExists = await Node.findOne({ $and: conditions });
-    if (!nodeExists) {
-      return;
-    } else {
-      // update surface objects
-      count = await NodeModel.updateOne(
-          { $and: conditions },
-          {
-            $push: { "usage":  newNodeUsage}
-          },
-          { new: true }
-      );
-
-    }
-    logger.debug("Stored Node Usage Object in DB", null);
-  } catch (err) {
-    logger.error(err.stack);
-  }
-  return 1;
-};
-
-
-module.exports = { saveCurrentUsageObject, saveDeployment, saveNode, saveNodeUsage };
+module.exports = { saveCurrentUsageObject, saveDeployment };
