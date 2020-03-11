7dda87190c0a54630cefa2f862834357de2562a5
Adam Yodinsky <adam@zooz.com>
2020-03-11 17:31:27 +0200

    data for cluster page is ready - moving to front end

---
+++ zeus-frontend/src/Components/Cluster/Cluster.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import nodeStyle from './Node.module.scss';
+import AreaGraph2 from '../AreaGraph/AreaGraph2';
+import axios from 'axios';
+
+class Cluster extends React.Component {
+  state = {};
+
+  componentDidMount() {
+    this.getNodeData(this.props).then((data) => {
+      if (data) {
+        this.setState({
+          real: data.usage,
+          formal: data.formal,
+        }, () => {
+          console.log(this.state);
+        });
+      }
+    });
+  }
+
+  getNodeData = async (props) => {
+    let usageResponse, formalResponse;
+    const usageUrl = `http://localhost:3001/clusterUsage`;
+    const formalUrl = `http://localhost:3001/clusterRequest`;
+
+    try {
+      usageResponse = axios.get(`${usageUrl}`);
+      formalResponse = axios.get(`${formalUrl}`);
+
+      // eslint-disable-next-line
+      await Promise.allSettled([usageResponse, formalResponse]).
+          then((result) => {
+            usageResponse = result[0].value.data.data;
+            formalResponse = result[1].value.data.data;
+          });
+    } catch (e) {
+      console.log('ERROR: could not get nodes state object');
+      console.log(e.stack);
+    }
+
+    return {
+      usage: usageResponse,
+      formal: formalResponse,
+    };
+  };
+
+  render() {
+    let areaGraph;
+
+    if (this.state.formal && this.state.real) {
+      areaGraph =
+          <section className={nodeStyle.box_graph}>
+            <AreaGraph2
+                formal={this.state.formal}
+                real={this.state.real}
+                name={this.props.state.name}
+                dataType={'cpu'}
+                stepSizeY={3600}
+                stepSizeX={5}
+            />
+            <div className={nodeStyle.separator}/>
+            <AreaGraph2
+                formal={this.state.formal}
+                real={this.state.real}
+                name={this.props.state.name}
+                dataType={'memory'}
+                stepSizeY={16000}
+                stepSizeX={5}
+            />
+          </section>;
+    }
+
+    return (
+
+        <div>
+          <div className={nodeStyle.box}>
+            <div className={nodeStyle.title_node}>{this.props.state.name}</div>
+            <section className={nodeStyle.box_graph}>
+              {areaGraph}
+            </section>
+          </div>
+        </div>
+    )
+        ;
+  }
+};
+
+export default Node;
---
+++ zeus-frontend/src/Components/Cluster/LineChart/LineChart.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import LineChart from '../../LineChart/LineChart';
+import areaGraph from './LineChart.module.scss';
+
+const lengthLimit = 500; // TODO make it an external config
+
+const getTitle = (props) => {
+  return props.dataType === 'cpu' ? 'CPU' : 'Memory';
+};
+
+const getDataLength = (props) => {
+  let dataLength;
+  if (props.formal.length > lengthLimit || props.real.length > lengthLimit) {
+    dataLength = lengthLimit;
+  } else {
+    dataLength = min(props.formal.length, props.real.length);
+  }
+
+  return dataLength;
+};
+
+
+const min = (a, b) => {
+  return a < b ? a : b;
+};
+
+const createDataSets = (props) => {
+  const requestArr = [];
+  const usageArr = [];
+  const timeArr = [];
+  const dataLength = getDataLength(props);
+
+  for (let i = 0; i < dataLength; i++) {
+    const tmpFormal = props.formal[i];
+    const date = new Date(tmpFormal.date);
+
+    let usage = (props.real[i])[props.dataType][0];
+    let request = tmpFormal.resources[props.dataType].request[0];
+
+    if (props.dataType === 'memory') {
+      request = Math.ceil(request / Math.pow(2, 20));
+    }
+
+    requestArr.unshift({
+          x: date,
+          y: request,
+        },
+    );
+
+    usageArr.unshift({
+      x: date,
+      y: usage,
+    });
+
+    timeArr.unshift(date);
+  }
+
+  return {
+    datasets: [
+      {
+        label: 'Request',
+        data: requestArr,
+        color: '#66b3ff', // blue
+      },
+      {
+        label: 'Usage',
+        data: usageArr,
+        color: '#ff6666', // red
+      }
+    ],
+    time: timeArr,
+    capacity: capacity
+  };
+};
+
+const LineChart = (props) => {
+  let data = createDataSets(props);
+  let title = getTitle(props);
+  // console.log(props);
+
+  return (
+      <div className={areaGraph.box}>
+        <LineChart
+            datasets={data.datasets}
+            time={data.time}
+            title={title}
+            capacity={data.capacity}
+            stepSizeY={props.stepSizeY}
+            stepSizeX={props.stepSizeX}
+        />
+      </div>
+  );
+};
+
+export default LineChart;
