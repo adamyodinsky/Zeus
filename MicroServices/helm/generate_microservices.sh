245c404ce8effcc42392b514020f653fadd35b29
Adam Yodinsky <adam@zooz.com>
2020-09-02 13:36:33 +0300

    microservices demo

--- MicroServices/helm/generate_microservices.sh
+++ MicroServices/helm/generate_microservices.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -e
-#set -x
-
-mem="${3}" || 128
-cpu="${4}" || 40
-replicas="${5}" || 1
+set -x
+mem=${3:-64}
+cpu=${4:-20}
+replicas="${5:-1}
 
 helm_template() {
   local letter="${1}"
